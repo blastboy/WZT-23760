397024df5ce4b341f2239ed7c72214cce2aedb43
ienovbz <160480666+ienovbz@users.noreply.github.com>
2024-02-22 13:49:37 +0100

    Update zoneminder_backup_update.sh

--- Zoneminder/zoneminder_backup_update.sh
+++ Zoneminder/zoneminder_backup_update.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Variables for date and time
-execute_date="19-02-2024"        # Date format is dd-mm-yyyy
-execute_time_backup="06:00"      # Time format is hh:mm
-execute_time_update="12:30"      # Time format is hh:mm
+execute_date="19-02-2024"                        # Date format is dd-mm-yyyy
+execute_time_backup="06:00"                      # Time format is hh:mm
+execute_time_update="12:30"                      # Time format is hh:mm
 
 # Logfile path for the main script
-log_file="/home/vanbreda/backup_script.log    # This is the location of the log-file
+log_file="/home/vanbreda/zoneminder_backup_update.log"      # This is the location of the log-file
 
 # Mounting info
-source_location="//10.23.10.11/C$/Backup/Zoneminder"  # A shared folder on a Windows-machine
-mount_point="/mnt/Backup"                             # The mounted location on the Zoneminder-machine
-username="username"                                   # The username of the Windows-machine
-password="password"                                   # The password of the user of Windows-machine
+source_location="//10.10.10.10/VanBreda/"        # A shared folder on a Windows-machine
+mount_point="/mnt/VanBreda"                      # The mounted location on the Zoneminder-machine
+destination_folder="Zoneminder1"                 # Modify this with the desired destination folder
+username="username"                              # The username of the Windows-machine
+password="password"                              # The password of the user of Windows-machine
 
 # Extracting hour and minute separately with leading zeros
 hour_backup=$(date -d "$execute_time_backup" '+%H')
@@ -33,24 +34,44 @@ display_help() {
     echo "zoneminder_update_backup.sh usage:"
     echo "-install              Setup cronjobs that executes the script with the argument -automated_backup and -automated_update"
     echo "-backup               Backup Zoneminder-settings to the Network-share"
-    echo "-update               Update the Zoneminder OS (excluding Zoneminder itself)"
+    echo "-update               Update the Zoneminder OS, excluding Zoneminder itself"
     echo "-automated_backup     Backup Zoneminder-settings to the Network-share on the specified day"
-    echo "-automated_update     Update the Zoneminder OS (excluding Zoneminder itself) on the specified day"
+    echo "-automated_update     Update the Zoneminder OS, excluding Zoneminder itself, on the specified day"
     echo "-uninstall            Remove the cronjob for automated execution"
     echo "-help                 Display this text"
 }
 
 # Function for backup
+# Function for backup
 backup() {
     log "Mounting backup location..."
 
-    # Check if the mount point is already mounted
-    if ! mountpoint -q "$mount_point"; then
-        log "Mount point is not already mounted. Attempting to mount."
-        mount -t cifs -o username="$username",password="$password" "$source_location" "$mount_point" && log "Mount successful."
-    else
-        log "Mount point is already mounted."
-    fi
+	# Create mount point if it doesn't exist
+	if [ ! -d "$mount_point" ]; then
+		log "Mount point does not exist. Creating $mount_point."
+		mkdir -p "$mount_point"
+	else
+		log "Mount point already exists."
+	fi
+
+	# Check if the mount point is already mounted
+	if ! mountpoint -q "$mount_point"; then
+		log "Mount point is not already mounted. Attempting to mount."
+		mount -t cifs -o username="$username",password="$password" "$source_location" "$mount_point" && log "Mount successful."
+	else
+		log "Mount point is already mounted."
+	fi
+
+	# Create destination folder if it doesn't exist
+	if [ ! -d "$mount_point/$destination_folder" ]; then
+		log "Destination folder does not exist. Creating $destination_folder."
+		mkdir -p "$mount_point/$destination_folder"
+	else
+		log "Destination folder already exists."
+	fi
+
+    # Set the destination folder for copying and exporting files
+    dest_folder="$mount_point/$destination_folder"
 
     # Check if the mount was successful before proceeding with copying files
     if mountpoint -q "$mount_point"; then
@@ -59,7 +80,7 @@ backup() {
         # Copy files to the mounted location if they exist
         for file in /etc/sysconfig/network-scripts/ifcfg-*; do
             if [ -e "$file" ]; then
-                cp "$file" "$mount_point" && log "Copied $file to $mount_point"
+                cp "$file" "$dest_folder" && log "Copied $file to $dest_folder"
             else
                 log "File $file not found. Skipped."
             fi
@@ -67,37 +88,37 @@ backup() {
 
         for file in /etc/sysconfig/network-scripts/route-*; do
             if [ -e "$file" ]; then
-                cp "$file" "$mount_point" && log "Copied $file to $mount_point"
+                cp "$file" "$dest_folder" && log "Copied $file to $dest_folder"
             else
                 log "File $file not found. Skipped."
             fi
         done
 
         if [ -e "/etc/netplan/00-installer-config.yaml" ]; then
-            cp "/etc/netplan/00-installer-config.yaml" "$mount_point" && log "Copied /etc/netplan/00-installer-config.yaml to $mount_point"
+            cp "/etc/netplan/00-installer-config.yaml" "$dest_folder" && log "Copied /etc/netplan/00-installer-config.yaml to $dest_folder"
         else
             log "File /etc/netplan/00-installer-config.yaml not found. Skipped."
         fi
 
         if [ -e "/etc/hosts" ]; then
-            cp "/etc/hosts" "$mount_point" && log "Copied /etc/hosts to $mount_point"
+            cp "/etc/hosts" "$dest_folder" && log "Copied /etc/hosts to $dest_folder"
         else
             log "File /etc/hosts not found. Skipped."
         fi
 
         if [ -e "/etc/resolv.conf" ]; then
-            cp "/etc/resolv.conf" "$mount_point" && log "Copied /etc/resolv.conf to $mount_point"
+            cp "/etc/resolv.conf" "$dest_folder" && log "Copied /etc/resolv.conf to $dest_folder"
         else
             log "File /etc/resolv.conf not found. Skipped."
         fi
 
         if [ -e "/etc/ntp.conf" ]; then
-            cp "/etc/ntp.conf" "$mount_point" && log "Copied /etc/ntp.conf to $mount_point"
+            cp "/etc/ntp.conf" "$dest_folder" && log "Copied /etc/ntp.conf to $dest_folder"
         else
             log "File /etc/ntp.conf not found. Skipped."
         fi
 
-        mysqldump --user=root --databases zm > "$mount_point/backup.SQL" && log "Mysqldump completed. Backup successful."
+        mysqldump --user=root --databases zm > "$dest_folder/backup.SQL" && log "Mysqldump completed. Backup successful."
     else
         log "Error: Mount point not available. Files and database not backed up."
     fi
@@ -195,7 +216,12 @@ case "$1" in
 
     -uninstall)
         log "Uninstalling cronjobs..."
-        # Remove existing cronjobs
+		echo "Your current cronjobs will be deleted"
+		echo "A backup will be saved to /home/vanbreda/root_cronjobs.txt"
+        crontab -l
+		crontab -l > /home/vanbreda/root_cronjobs.txt
+		log "Cronjobs saved to /home/vanbreda/root_cronjobs.txt"
+		# Remove existing cronjobs
         crontab -r
         log "Cronjobs uninstalled successfully."
         ;;
